8778f642835a346d574f0d8afa367ff345f46b96
Craig Freeman <cfreeman@kenzan.com>
2018-08-23 11:57:22 -0600

    Removed protocol from env variable

--- ui/src/server/api.js
+++ ui/src/server/api.js
@@ -1,7 +1,7 @@
 const fetch = require('node-fetch');
 const { API_PORT, API_URL } = require('./config');
 
-exports.getFramework = () => fetch(`${API_URL}:${API_PORT}/random`)
+exports.getFramework = () => fetch(`http://${API_URL}:${API_PORT}/random`)
   .then(res => {
     if (res.status !== 200) {
       throw Error(res.message);
--- ui/src/server/config.js
+++ ui/src/server/config.js
@@ -1,2 +1,2 @@
-exports.API_URL = process.env.API_URL || 'http://localhost';
+exports.API_URL = process.env.API_URL || 'localhost';
 exports.API_PORT = process.env.API_PORT || '5001';
